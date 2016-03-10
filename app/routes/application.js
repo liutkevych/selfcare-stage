4e6b645b4ea6f794abb3bf515858c0e259b6e760
cybdoom <cybdoom@gmail.com>
2016-03-10 12:33:52 +0200

    Added .DS_Store to gitignore

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,8 @@
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-var ApplicationRoute = Em.Route.extend(ApplicationRouteMixin, {
+var ApplicationRoute = Ember.Route.extend(ApplicationRouteMixin, {
+  session: Ember.inject.service(),
+
   actions: {
     invalidateSession: function() {
       this.get('session').invalidate();
