04413c32c72c17a59b52ee841ab7e0c2d400e73e
liutkevych <oleksandr.liutkevych@seniordevonly.com>
2017-07-24 16:28:32 +0300

    Created model for campaignfilters

---
+++ app/models/campaignfilter.js
@@ -0,0 +1,16 @@
+import DS from 'ember-data';
+import Ember from 'ember';
+
+
+export default DS.Model.extend({
+  locationId:               DS.attr(),
+  kind:                     DS.attr(),
+  gender:                   DS.attr(),
+  max_age:                  DS.attr(),
+  min_age:                  DS.attr(),
+  times_visited:            DS.attr(),
+  date_number:              DS.attr(),
+  date_type:                DS.attr(),
+  targets_count:            DS.attr(),
+  total_count:              DS.attr()
+});
